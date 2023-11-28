9c87af7e1bc0ad010304c5708c13e4c68c7f81de
Werner Glinka <werner@glinka.co>
2023-11-28 11:35:50 -0600

    initial commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,18 @@
+import { useLoaderData } from "@remix-run/react";
+import electron from "~/electron.server";
+
+export function loader() {
+  return {
+    userDataPath: electron.app.getPath("userData"),
+  };
+}
+
+export default function Index() {
+  const data = useLoaderData<typeof loader>();
+  return (
+    <main>
+      <h1>Welcome to Remix</h1>
+      <p>User data path: {data.userDataPath}</p>
+    </main>
+  );
+}
